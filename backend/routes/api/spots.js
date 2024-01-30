467e027a5a7befef3e2251cbbb897c24cf66ba48
Guoxin <cindygxli@gmail.com>
2024-01-30 14:47:46 -0800

    4 endpoints completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const router = express.Router();
+
 const bcrypt = require('bcryptjs');
 const { Op } = require('sequelize');
-const requireAuth = require("../../utils/auth")
+
+const { requireAuth } = require("../../utils/auth")
 const { Spot, SpotImage, User, Booking, Review, ReviewImage } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const router = express.Router();
+
 
 //spot validation
 const validateSpot = [
@@ -52,6 +55,7 @@ const validateSpot = [
 ];
 
 
+
 //get all spots owned by the current user - URL: /api/spots/current
 router.get('/current', requireAuth, async (req, res) => {
     const userId = req.user.id;
@@ -72,27 +76,101 @@ router.get('/current', requireAuth, async (req, res) => {
 
         const previewImage = await SpotImage.findOne({
             attributes: ['url'],
-            where: { spotId: spot.id, preview: true }
+            where: { spotId: spot.id }
         });
 
-        const formattedSpot = spot.toJSON();
+        const formatSpot = spot.toJSON();
 
         if (avgRating) {
-            formattedSpot.avgRating = avgRating;
+            formatSpot.avgRating = avgRating;
         } else {
-            formattedSpot.avgRating = null;
-        }
-
+            formatSpot.avgRating = null;
+        };
         if (previewImage) {
-            formattedSpot.previewImage = previewImage.url;
+            formatSpot.previewImage = previewImage.url;
         } else {
-            formattedSpot.previewImage = null;
-        }
-        return formattedSpot;
+            formatSpot.previewImage = null;
+        };
+        return formatSpot;
     }));
 
     res.status(200).json({ Spots: arr })
+});
+
+// get details of a spot from an id - URL: /api/spots/:spotId
+router.get('/:spotId', async (req, res, next) => {
+    const { spotId } = req.params;
+    let spot = await Spot.findByPk(spotId);
+
+    if (!spot) {
+        res.status(404).json({ message: "Spot not found." });
+    }
+
+    const numReviews = await Review.count({
+        where: { spotId }
+    });
+
+    const totalRating = await Review.sum('stars', {
+        where: { spotId }
+    });
+
+    const avgRating = totalRating / numReviews;
+
+    spot = spot.toJSON();
+    spot.numReviews = numReviews;
+    spot.avgRating = avgRating;
+    spot.spotImage = await SpotImage.findAll({
+        attributes: ['id', 'url', 'preview'],
+        where: { spotId, preview: true }
+    });
+    spot.Owner = await User.findByPk(spot.ownerId, {
+        attributes: {
+            exclude: ['username']
+        }
+    })
+    res.json(spot)
 })
 
+//edit a spot - URL: /api/spots/:spotId
+router.put('/:spotId', requireAuth, validateSpot, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const spotId = req.params.spotId;
+    const user = req.user;
+    const spot = await Spot.findByPk(spotId);
+
+    if (!spot) {
+        return res.status(404).json({ message: "Spot not found." })
+    };
+
+    if (user.id === spot.ownerId) {
+        spot.address = address;
+        spot.city = city;
+        spot.state = state;
+        spot.country = country;
+        spot.lat = lat;
+        spot.lng = lng;
+        spot.name = name;
+        spot.description = description;
+        spot.price = price;
+
+        await spot.save()
+        res.status(200).json(spot)
+    }
+});
+
+//delete a spot - URL: /api/spots/:spotId
+router.delete('/:spotId', requireAuth, async (req, res, next) => {
+    const { spotId } = req.params;
+
+    const spot = await Spot.findByPk(spotId);
+
+    if (!spot) {
+        return res.status(404).json({ message: "Spot not found." })
+    };
+
+    await spot.destroy();
+    return res.json({ message: "Spot deleted sucessfully." })
+
+});
 
 module.exports = router;
