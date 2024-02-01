b23e77a44a00ef0f82cb55b426e1bdadbe79f1f5
Guoxin <cindygxli@gmail.com>
2024-01-31 18:02:05 -0800

    booking routes in progress

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -11,30 +11,115 @@ const router = express.Router();
 
 
 //get all of the current user's bookings - URL: /api/bookings/current
-router.get('/current', requireAuth, async (req, res) => {
-    const userId = req.user.id;
+router.get('/current', requireAuth, async (req, res, next) => {
+    const { id } = req.user;
+    const returnArr = [];
+
     const bookings = await Booking.findAll({
-        where: { userId: userId },
+        where: { userId: id },
         include: [
             {
                 model: Spot,
-                as: 'Spot',
-                attributes: ['id', 'name', 'address', 'city', 'state', 'country', 'price', 'previewImage']
+                attributes: {
+                    exclude: ['description', 'createdAt', 'updatedAt']
+                }
+            }]
+    })
+
+    for (let i = 0; i < bookings.length; i++) {
+        let booking = bookings[i];
+
+        const previewImage = await SpotImage.findOne({
+            where: { spotId: booking.spotId }
+        })
+
+        booking = booking.toJSON();
+        if (previewImage) {
+            booking.Spot.previewImage = previewImage.url;
+        } else {
+            booking.Spot.previewImage = null;
+        }
+
+        returnArr.push(booking);
+    }
+
+    return res.status(200).json({ Bookings: returnArr })
+
+});
+
+
+//edit a booking - URL: /api/bookings/:bookingId
+router.put('/:bookingId', requireAuth, async (req, res) => {
+    const { startDate, endDate } = req.body;
+    const { bookingId } = req.params;
+    const { user } = req;
+    const booking = await Booking.findByPk(bookingId);
+
+    if (!booking) {
+        return res.status(404).json({ message: "Booking couldn't be found" })
+    }
+
+    let startDateInput = Date.parse(startDate);
+    let endDateInput = Date.parse(endDate);
+
+    if (startDateInput <= endDateInput) {
+        return res.status(400).json({
+            message: "Bad Request",
+            errors: { endDate: "endDate cannot be on or before startDate" }
+        })
+    }
+
+    const dateNow = Date.now()
+
+    if (dateNow > Date.parse(booking.endDate)) {
+        return res.status(403).json({ message: "Past bookings can't be modified" })
+    }
+
+    const currentBooking = await Booking.findAll({
+        where: { spotId: booking.spotId }
+    })
+
+    for (let i = 0; i < currentBooking.length; i++) {
+        let currentBooking = currentBooking[i];
+        currentStartDate = Date.parse(currentBooking.startDate);
+        currentEndDate = Date.parse(currentBooking.endDate);
+
+        if (currentBooking.id !== booking.id) {
+
+            if ((startDateInput <= currentEndDate && startDateInput >= currentStartDate && currentEndDate >= currentStartDate && currentEndDate <= currentEndDate) || (startDateInput <= currentStartDate && endDateInput >= currentEndDate)) {
+                return res.status(403).json({
+                    message: "Sorry, this spot is already booked for the specified dates",
+                    errors: {
+                        startDate: "Start date conflicts with an existing booking",
+                        endDate: "End date conflicts with an existing booking"
+                    }
+                })
             }
-        ]
-    });
 
+            if (startDateInput <= currentEndDate && measuredStartDate >= currentStartDate) {
+                return res.status(403).json({
+                    message: "Sorry, this spot is already booked for the specified dates",
+                    errors: { startDate: "Start date conflicts with an existing booking" }
+                })
+            }
 
+            if (endDateInput >= currentStartDate && endDateInput <= currentEndDate) {
+                return res.status(403).json({
+                    message: "Sorry, this spot is already booked for the specified dates",
+                    errors: { endDate: "End date conflicts with an existing booking" }
+                })
+            }
 
-    return res.status(200).json
-})
+        }
+    }
+    if (user.id === booking.userId) {
+        booking.startDate = startDate;
+        booking.endDate = endDate;
 
-
-
-
-
-
-
+        await booking.save();
+        return res.status(200).json(booking);
+    }
+});
 
 
 
