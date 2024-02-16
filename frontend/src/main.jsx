bf963f6d2e2aaefd9ee2dc2671c0855a9417b8e5
Guoxin <cindygxli@gmail.com>
2024-02-16 14:05:19 -0800

    frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import configureStore from './store/store.js';
+import App from './App';
+import './index.css';
+import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import { ModalProvider, Modal } from './context/Modal';
 
 const store = configureStore();
 
-if (import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
+// const Carrot = () => (
+//   <div style={{ color: "orange", fontSize: "100px" }}>
+//     <i className="fas fa-carrot"></i>
+//   </div>
+// );
 
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        {/* <Carrot /> */}
+        {/* <Modal /> */}
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
